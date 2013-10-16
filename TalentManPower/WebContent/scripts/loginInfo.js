9a023fc105e7079a23bcf50fa12c5307032cfc36
sudiksha <sudiksha@sudiksha-PC>
2013-10-16 14:07:38 +0530

    some hngesss

--- TalentManPower/WebContent/scripts/loginInfo.js
+++ TalentManPower/WebContent/scripts/loginInfo.js
@@ -1,12 +1,12 @@
 
-
 var APP_PATH = "/tmp/";
 
 $(document).ready(function() {
 	initiateLoginBox();
 	$("#UserInformation").hide();
-	$("#logOut").hide();
+	$("nav3").hide();
 	initiateForgotPwd();
+	$("#forgotPwdDetails").hide();
 	$.ajax({
 		type:"POST",
 		url: APP_PATH + "/views/buttonCheck.jsp",
@@ -17,7 +17,7 @@ $(document).ready(function() {
 			if(responseMessage==1 || responseMessage==2) {
 				console.log("resp2");
 				$("nav2").hide();
-				$("#logOut").show();
+				$("nav3").show();
 				initiateUserInfoBox();
 			}
 		}
@@ -28,7 +28,39 @@ $(document).ready(function() {
 
 function initiateForgotPwd() {
 	$("#forgotPwd").click(function() {
-		
+		$("#forgotPwdDetails").show();
+		$("#forgotPwdBtn").click(function(){
+			var ema=$("#PwdemailId").val();
+			alert(ema);
+			if(ema=="" || ema==$("#forgotPwdBtn").attr('placeholder')){
+				$("#loginErrorMessage").html("Enter EmailId correctly");
+				$("#loginErrorMessage").show();
+			}
+			
+			var forgotPwd=$("#forgotPwdForm");
+			$.ajax({
+				type:"POST",
+				url: APP_PATH+'bridge',
+				data: forgotPwd.serialize(),
+				success: function(response){
+					//alert("Response is: "+response);
+					var responseMessage = $.trim(response);
+					//alert("msg received is ::  "+responseMessage);
+					
+					if(responseMessage != "1" ) {
+						alert("Inside if");
+						$("#loginErrorMessage").html("Email id not valid.");
+						$("#loginErrorMessage").show();
+					} else {
+				//	alert("After Submit");
+						$("#PwdemailId").html("");
+						$("#forgotPwdDetails").hide();
+					}											
+				}
+			  });
+			
+
+		});
 	});
 }
 
@@ -43,6 +75,7 @@ function initiateLoginBox() {
 	$("#loginUserName").keypress(function(e) {
 		if(e.keyCode == 13 || e.which == 13) {
 			$("#userLoginButton").click();
+			
 	}
 	});
 	
@@ -91,13 +124,13 @@ function initiateLoginBox() {
 				
 				if(responseMessage != "1" ) {
 					alert("Inside if");
+					$("#loginErrorMessage").html("Invalid UserName and password");
 					$("#loginErrorMessage").show();
 				} else {
-			//	alert("After Submit");
+				alert("After Submit");
+					$("nav2").hide();
 					$("#loginUserName").html("");
 					$("#loginUserPassword").html("");
-					$("nav2").hide();
-					
 				initiateUserInfoBox();
 				}											
 			}
@@ -115,7 +148,7 @@ function initiateUserInfoBox() {
 	$("#UserInformation").load(APP_PATH+"/views/dashBoard.jsp", function(response,status,xhr){console.log("information extracted");});
 	console.log($("#UserInformation").text());
 	$("#UserInformation").show();
-	$("#logOut").show();
+	$("nav3").show();
 	
 	console.log("info retrieved");
 	
@@ -126,10 +159,52 @@ function initiateUserInfoBox() {
 			success: function(response){
 				console.log("session invalidated");
 				$("nav2").show();
-				$("#logOut").hide();
+				$("nav3").hide();
 				$("#UserInformation").hide();
 			}
 		});
 	});
 	
+	$("#changePwdBtn").click(function(){
+		
+		var password=$("#newPwd").val();  
+		if(password=="" || password==$("#newPwd").attr('placeholder')){
+		//  $("#email_error_AF").html("This field is required");
+		 $("#changePwdFailureDiv").html("enter password correctly");
+		 $("#changePwdFailureDiv").focus();
+		}
+		else if(password.length<6){
+			//  $("#email_error_AF").html("This field is required");
+			$("#changePwdFailureDiv").html("password must be of length more than 6 letters");
+			 $("#newPwd").focus();
+			 $("#changePwdFailureDiv").show();
+		}
+		else{
+		
+		var changePwdForm=$("#changePwdFrm");
+		$.ajax({
+			type:"POST",
+			url: APP_PATH+'bridge',
+			data: changePwdForm.serialize(),
+			success: function(response){
+				//alert("Response is: "+response);
+				var responseMessage = $.trim(response);
+				
+				
+				if(responseMessage != "1" ) {
+					alert("Inside if");
+					$("#changePwdFailureDiv").html("error in changing Password");
+					$("#changePwdFailureDiv").show();
+				} else {
+				alert("After Submit");
+					$("#newPwd").html("");
+				}											
+			}
+		  });
+		
+		}
+	});
+		
+		
+
 }
